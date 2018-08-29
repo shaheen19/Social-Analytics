9ae40a127eaa47550be2bf7b6bb7e8fd509cd564
Ruby <Robina@ucsd.edu>
2018-08-29 02:04:06 -0700

    Remove API keys

--- Social-Analytics/config1.py
+++
@@ -1,6 +0,0 @@
-# Twitter API Keys
-consumer_key = "xGaknYVONq0fcr1jj4GOjKsPO"
-consumer_secret = "J4djTsjFTurPYlbyGPjyeMe8vtP1xZL9f9FNiWvwTiJk8MeFuj"
-access_token = "J4djTsjFTurPYlbyGPjyeMe8vtP1xZL9f9FNiWvwTiJk8MeFuj"
-access_token_secret = "W8YP6Mz0jtzCbQCP7az5O8pdRr2fh68rYSyIaLWBgofpT"
-

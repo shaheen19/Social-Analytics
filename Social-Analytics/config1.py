d61efdb0d7060fcdd5bd3b5adc03226041585bfd
Ruby <Robina@ucsd.edu>
2018-08-28 22:41:31 -0700

    analysis included with percent polarity

---
+++ Social-Analytics/config1.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "xGaknYVONq0fcr1jj4GOjKsPO"
+consumer_secret = "J4djTsjFTurPYlbyGPjyeMe8vtP1xZL9f9FNiWvwTiJk8MeFuj"
+access_token = "J4djTsjFTurPYlbyGPjyeMe8vtP1xZL9f9FNiWvwTiJk8MeFuj"
+access_token_secret = "W8YP6Mz0jtzCbQCP7az5O8pdRr2fh68rYSyIaLWBgofpT"
+
